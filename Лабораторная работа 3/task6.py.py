d15665ea02205ead3f7284fe832ff79ee6caae49
Chertk <115380159+Chertk@users.noreply.github.com>
2022-10-16 23:47:04 +0300

    Add files via upload
    Сложности возникли из-за ошибки в задании, но на лекции вроде с ней разобрались

---
+++ Лабораторная работа 3/task6.py.py
@@ -0,0 +1,14 @@
+list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+max_index = 0
+for i, current_number in enumerate(list_numbers):
+    max_number = list_numbers[max_index]
+    if current_number > max_number:
+        max_index = i
+        max_number = list_numbers[max_index]
+
+list_ = list_numbers[-1]
+list_numbers[max_index] = list_
+list_numbers[-1] = max_number
+
+print(list_numbers)
